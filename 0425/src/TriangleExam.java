ff52e1c11822607a7e8ebc822a2c2cc779cbaf37
Administrator <Administrator@501-S16>
2022-04-26 09:49:38 +0900

    0426 프로젝트 추가

---
+++ 0425/src/TriangleExam.java
@@ -0,0 +1,27 @@
+
+public class TriangleExam {
+
+	public static void main(String[] args) {
+		Triangle tr = new Triangle(10.2 , 17.3);
+		System.out.println("산각형의 넓이: "+ tr.getArea());
+		tr.setSize(7.5, 9.2);
+		System.out.println("변경된 삼각형의 넓이: "+ tr.getArea());
+	}
+
+}
+class Triangle {
+	double bottom;
+	double height;
+	public Triangle(double bt, double hg) {
+		this.bottom = bt;
+		this.height = hg;
+	}
+	public void setSize(double d, double e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public double getArea() {
+		return bottom*height/2;
+		
+	}
+}
