0c9f6e22e002a9a290be8c3b3ba9627ce74708a2
Administrator <Administrator@501-S16>
2022-05-31 10:25:46 +0900

    0531 프로젝트 추가

--- 0531/src/Test.java
+++ 0531/src/Test.java
@@ -1,9 +1,27 @@
-
+import java.util.Scanner;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+			System.out.println("두 정수와 연산자를 입력하시오>>");
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			char operator = scanner.next().charAt(0);
+			switch (operator ) {
+			case '+':
+				System.out.println(a+b);
+				break;
+			case '-':
+				System.out.println(a-b);
+				break;
+			case '*':
+				System.out.println(a*b);
+				break;
+			case '%':
+				System.out.println(a%b);
+				break;
+			}
+			
 	}
 
 }
